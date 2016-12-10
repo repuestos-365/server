1fa50d9e410568b34f751a1db57524d8e57597f5
guillen-jorge88 <guillen.jorge88@gmail.com>
2016-12-10 16:50:08 -0400

    mlab connect

--- server.js
+++ server.js
@@ -26,8 +26,10 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 console.log('1-mongoURL: ' + mongoURL);
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
     var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-        mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-        mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+        //mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+        mongoHost = process.env.MONGO_HOST,
+        //mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+        mongoPort = process.env.MONGO_PORT,
         mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
         mongoPassword = process.env[mongoServiceName + '_PASSWORD']
     mongoUser = process.env[mongoServiceName + '_USER'];
