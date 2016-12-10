ff406ae1dedcef6d2ac321a8b99802f3081ea3ea
guillen-jorge88 <guillen.jorge88@gmail.com>
2016-12-10 18:20:30 -0400

    mlab connect  Environment successful

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
-console.log('1-mongoURL: ' + mongoURL);
+//console.log('1-mongoURL: ' + mongoURL);
 if (mongoURL == null && process.env.MLAB_SERVICE_NAME) {
     /*var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
         mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
@@ -51,7 +51,7 @@ if (mongoURL == null && process.env.MLAB_SERVICE_NAME) {
         // Provide UI label that excludes user id and pw
         mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
         mongoURL += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-        console.log('2-mongoURL: ' + mongoURL);
+        //console.log('2-mongoURL: ' + mongoURL);
     }
 }
 var db = null,
